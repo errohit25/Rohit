b39dda02b93c3f2b0418b49b3b830eeec5df9fbe
ER_Rohit _chavan <rohitbchavan2425@gmail.com>
2025-08-28 15:25:14 +0530

    add css and js

---
+++ script.js
@@ -0,0 +1,8 @@
+function openbtn() {
+    document.getElementById("sidebar").style.width = "500px";
+    document.getElementById("openbtn").style.display = "none";
+}
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("openbtn").style.display = "block";
+}
